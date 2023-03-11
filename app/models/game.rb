7ece704e239f08c761c64b026b6e6feb5a44e3ff
prince475 <danielmwai475@gmail.com>
2023-03-11 05:05:12 +0300

    Finished reviewing the one to many relations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
+
+  # using the has_many mcro is the same as writting th instance method below.
+  # def reviews
+  #   Review.where(game_id: self.id)
+  # end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ActiveRecord::Base
-  
+  belongs_to :game
+
+  # using a belongs_to is just the same as writting our instance get method below
+
+  # # areview belongs to a game
+  # def game
+  #   # self is the review instance.
+  #   Game.find(self.game_id)
+  # end
 end
